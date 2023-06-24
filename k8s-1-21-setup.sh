df532c864619978c0d0c774873a7a12eaf8cf3ec
Open Source book <amitganvir6@gmail.com>
2023-06-25 00:21:31 +0530

    Update k8s-1-21-setup.sh
    new

--- k8s-1-21-setup.sh
+++ k8s-1-21-setup.sh
@@ -5,9 +5,9 @@
 ## Master Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.medium - create key pair -network setting , security group set to allow all traffic as of now from anywhere  --> launch instace
 ## Worker Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.micro - use existing key pair for master -network setting  , use existing security group for master - instance number 2 --> launch instace
 
-
+Step1:
+## Master and Worker Side
 sudo apt update
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common  -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
@@ -21,13 +21,24 @@ apt install kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 -y
 sudo apt-mark hold kubelet kubeadm kubectl
 sysctl net.bridge.bridge-nf-call-iptables=1
 
-#on master
+Step2:
+# On master Side
 #kubeadm init --pod-network-cidr=10.244.0.0/16 >> cluster_initialized.txt
 kubeadm init --pod-network-cidr=192.168.0.0/16 >> cluster_initialized.txt
 mkdir /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+#kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/archive/v3.20/manifests/calico.yaml
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 systemctl restart kubelet.service
 kubeadm token create --print-join-command
+
+Step3:
+#On Worker Side
 ## Execute output of 'kubeadm token' on Worker node
+
+Step4:
+# On Master Side
+###
+kubect get nodes
+
