27dc067aacd1cc3dd5efa90c6b03e402c3fd0a70
Open Source book <amitganvir6@gmail.com>
2023-06-27 17:47:22 +0530

    Update k8s-1-21-setup.sh

--- k8s-1-21-setup.sh
+++ k8s-1-21-setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash 
 
-## On Worker and Master Node
 
-## Master Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.medium - create key pair -network setting , security group set to allow all traffic as of now from anywhere  --> launch instace
-## Worker Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.micro - use existing key pair for master -network setting  , use existing security group for master - instance number 2 --> launch instace
+# Step1: ---------------------------------------------------------------------------------------------------------------------------------
+Create Security Group and all all Traffic port as of now on temprory bases. Use this Security Group while launching Maseter and Worker ec2.
 
-Step1:
-## Master and Worker Side
+#Step2: ---------------------------------------------------------------------------------------------------------------------------------
+##### Launche ec2 instances
+###### Use your Security Group while luanching and your keypair
+# 1) ec2 master Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.medium - create key pair -network setting , security group set to allow all traffic as of now from anywhere  --> launch instace
+# 2) ec2 Worker Node Configuration - instance name : Ubuntu - ubuntu20.04  - t2.micro - use existing key pair for master -network setting  , use existing security group for master - instance number 2 --> launch instace
+
+
+#
+
+# Step3: ---------------------------------------------------------------------------------------------------------------------------------
+##### Putty(ssh) Master and Worker ec2 and perofrm below commands on both - Login with root user #####
+sudo su -
+
+apt-get update -y
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common  -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -21,24 +32,46 @@ apt install kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 -y
 sudo apt-mark hold kubelet kubeadm kubectl
 sysctl net.bridge.bridge-nf-call-iptables=1
 
-Step2:
-# On master Side
-#kubeadm init --pod-network-cidr=10.244.0.0/16 >> cluster_initialized.txt
+
+# Step4: # On master Side ---------------------------------------------------------------------------------------------------------------------------------
+sudo su -
 kubeadm init --pod-network-cidr=192.168.0.0/16 >> cluster_initialized.txt
 mkdir /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
 #kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl apply -f https://docs.projectcalico.org/archive/v3.20/manifests/calico.yaml
+#kubectl apply -f https://docs.projectcalico.org/archive/v3.20/manifests/calico.yaml
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
 systemctl restart kubelet.service
 kubeadm token create --print-join-command
 
-Step3:
-#On Worker Side
-## Execute output of 'kubeadm token' on Worker node
 
-Step4:
-# On Master Side
-###
-kubect get nodes
+Step5: ---------------------------------------------------------------------------------------------------------------------------------
+#On Worker Side
+## Execute 'kubeadm token' command output from master node and run it in Worker node. So that it will join to as a Worker node in k8s cluster
+For example: ##kubeadm join 172.31.22.254:6443 --token t5wam7.35gqolngm80zf9di --discovery-token-ca-cert-hash sha256:60b88e1203525836853e98022aefe92348262bf8e63986dece6bb8270209daf4
+
+Step6: ---------------------------------------------------------------------------------------------------------------------------------
+# On Master Side, now execute below command to check cluster nodes.
+kubectl get nodes
 
